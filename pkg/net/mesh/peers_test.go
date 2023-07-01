cbd3b6c15d2cff4142d29c1a4b66b65adb57fa9d
Avi Zimmerman <avi.zimmerman@gmail.com>
2023-07-01 23:18:54 +0300

    More test cases

--- pkg/net/mesh/peers_test.go
+++ pkg/net/mesh/peers_test.go
@@ -20,11 +20,14 @@ import (
 	"context"
 	"net/netip"
 	"reflect"
+	"sort"
 	"testing"
 
+	v1 "github.com/webmeshproj/api/v1"
 	"golang.zx2c4.com/wireguard/wgctrl/wgtypes"
 
 	"github.com/webmeshproj/node/pkg/meshdb"
+	"github.com/webmeshproj/node/pkg/meshdb/networking"
 	"github.com/webmeshproj/node/pkg/meshdb/peers"
 )
 
@@ -36,7 +39,7 @@ func TestWireGuardPeers(t *testing.T) {
 		wantIPs map[string]map[string][]netip.Prefix // peerID -> peerID -> []allowed ips
 	}{
 		{
-			name: "simple",
+			name: "simple 1-to-1",
 			peers: map[string][]netip.Prefix{
 				"peer1": {
 					netip.MustParsePrefix("172.16.0.1/32"),
@@ -65,6 +68,135 @@ func TestWireGuardPeers(t *testing.T) {
 				},
 			},
 		},
+		{
+			name: "simple 1-to-1-to-1",
+			peers: map[string][]netip.Prefix{
+				"peer1": {
+					netip.MustParsePrefix("172.16.0.1/32"),
+					netip.MustParsePrefix("2001:db8::1/128"),
+				},
+				"peer2": {
+					netip.MustParsePrefix("172.16.0.2/32"),
+					netip.MustParsePrefix("2001:db8::2/128"),
+				},
+				"peer3": {
+					netip.MustParsePrefix("172.16.0.3/32"),
+					netip.MustParsePrefix("2001:db8::3/128"),
+				},
+			},
+			edges: map[string][]string{
+				"peer1": {"peer2", "peer3"},
+				"peer2": {"peer1", "peer3"},
+				"peer3": {"peer1", "peer2"},
+			},
+			wantIPs: map[string]map[string][]netip.Prefix{
+				"peer1": {
+					"peer2": {
+						netip.MustParsePrefix("172.16.0.2/32"),
+						netip.MustParsePrefix("2001:db8::2/128"),
+					},
+					"peer3": {
+						netip.MustParsePrefix("172.16.0.3/32"),
+						netip.MustParsePrefix("2001:db8::3/128"),
+					},
+				},
+				"peer2": {
+					"peer1": {
+						netip.MustParsePrefix("172.16.0.1/32"),
+						netip.MustParsePrefix("2001:db8::1/128"),
+					},
+					"peer3": {
+						netip.MustParsePrefix("172.16.0.3/32"),
+						netip.MustParsePrefix("2001:db8::3/128"),
+					},
+				},
+				"peer3": {
+					"peer1": {
+						netip.MustParsePrefix("172.16.0.1/32"),
+						netip.MustParsePrefix("2001:db8::1/128"),
+					},
+					"peer2": {
+						netip.MustParsePrefix("172.16.0.2/32"),
+						netip.MustParsePrefix("2001:db8::2/128"),
+					},
+				},
+			},
+		},
+		{
+			name: "simple site-to-site",
+			peers: map[string][]netip.Prefix{
+				"site1-router": {
+					netip.MustParsePrefix("172.16.0.1/32"),
+					netip.MustParsePrefix("2001:db8::1/128"),
+				},
+				"site2-router": {
+					netip.MustParsePrefix("172.16.0.2/32"),
+					netip.MustParsePrefix("2001:db8::2/128"),
+				},
+				"site1-follower": {
+					netip.MustParsePrefix("172.16.0.3/32"),
+					netip.MustParsePrefix("2001:db8::3/128"),
+				},
+				"site2-follower": {
+					netip.MustParsePrefix("172.16.0.4/32"),
+					netip.MustParsePrefix("2001:db8::4/128"),
+				},
+			},
+			edges: map[string][]string{
+				"site1-router": {"site2-router", "site1-follower"},
+				"site2-router": {"site1-router", "site2-follower"},
+			},
+			wantIPs: map[string]map[string][]netip.Prefix{
+				"site1-router": {
+					"site2-router": {
+						netip.MustParsePrefix("172.16.0.2/32"),
+						netip.MustParsePrefix("2001:db8::2/128"),
+						// site2-follower is reachable via site2-router
+						netip.MustParsePrefix("172.16.0.4/32"),
+						netip.MustParsePrefix("2001:db8::4/128"),
+					},
+					"site1-follower": {
+						netip.MustParsePrefix("172.16.0.3/32"),
+						netip.MustParsePrefix("2001:db8::3/128"),
+					},
+				},
+				"site2-router": {
+					"site1-router": {
+						netip.MustParsePrefix("172.16.0.1/32"),
+						netip.MustParsePrefix("2001:db8::1/128"),
+						// site1-follower is reachable via site1-router
+						netip.MustParsePrefix("172.16.0.3/32"),
+						netip.MustParsePrefix("2001:db8::3/128"),
+					},
+					"site2-follower": {
+						netip.MustParsePrefix("172.16.0.4/32"),
+						netip.MustParsePrefix("2001:db8::4/128"),
+					},
+				},
+				"site1-follower": {
+					"site1-router": {
+						// All IPs reachable via site1-router
+						netip.MustParsePrefix("172.16.0.1/32"),
+						netip.MustParsePrefix("2001:db8::1/128"),
+						netip.MustParsePrefix("172.16.0.2/32"),
+						netip.MustParsePrefix("2001:db8::2/128"),
+						netip.MustParsePrefix("172.16.0.4/32"),
+						netip.MustParsePrefix("2001:db8::4/128"),
+					},
+				},
+				"site2-follower": {
+					"site2-router": {
+						// All IPs reachable via site2-router
+						netip.MustParsePrefix("172.16.0.1/32"),
+						netip.MustParsePrefix("2001:db8::1/128"),
+						netip.MustParsePrefix("172.16.0.2/32"),
+						netip.MustParsePrefix("2001:db8::2/128"),
+						netip.MustParsePrefix("172.16.0.3/32"),
+						netip.MustParsePrefix("2001:db8::3/128"),
+					},
+				},
+			},
+		},
 	}
 
 	for _, tc := range tt {
@@ -75,6 +207,19 @@ func TestWireGuardPeers(t *testing.T) {
 				t.Fatalf("create test db: %v", err)
 			}
 			peerdb := peers.New(db)
+			nw := networking.New(db)
+			// Create an allow-all traffic policy.
+			err = nw.PutNetworkACL(ctx, &v1.NetworkACL{
+				Name:             "allow-all",
+				Action:           v1.ACLAction_ACTION_ACCEPT,
+				SourceNodes:      []string{"*"},
+				DestinationNodes: []string{"*"},
+				SourceCidrs:      []string{"*"},
+				DestinationCidrs: []string{"*"},
+			})
+			if err != nil {
+				t.Fatalf("put network acl: %v", err)
+			}
 			for peerID, addrs := range tc.peers {
 				_, err = peerdb.Put(ctx, &peers.PutOptions{
 					ID:        peerID,
@@ -109,12 +254,15 @@ func TestWireGuardPeers(t *testing.T) {
 					t.Fatalf("get peers for %q: %v", peer, err)
 				}
 				got := make(map[string][]netip.Prefix)
+				for node, ips := range want {
+					want[node] = sortPrefixes(ips)
+				}
 				for _, p := range peers {
 					var ips []netip.Prefix
 					for _, ip := range p.AllowedIps {
 						ips = append(ips, netip.MustParsePrefix(ip))
 					}
-					got[p.Id] = ips
+					got[p.Id] = sortPrefixes(ips)
 				}
 				if !reflect.DeepEqual(got, want) {
 					t.Errorf("got %v, want %v", got, want)
@@ -123,6 +271,15 @@ func TestWireGuardPeers(t *testing.T) {
 			teardown()
 		})
 	}
+}
+
+func sortPrefixes(in []netip.Prefix) []netip.Prefix {
+	out := make([]netip.Prefix, len(in))
+	copy(out, in)
+	sort.Slice(out, func(i, j int) bool {
+		return out[i].String() < out[j].String()
+	})
+	return out
 }
 
 func mustGenerateKey(t *testing.T) wgtypes.Key {
