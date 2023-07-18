05598cd9b7e08203644cdfc06cca43baf2a739b5
Avi Zimmerman <avi.zimmerman@gmail.com>
2023-07-19 01:38:03 +0300

    Add version command

---
+++ pkg/ctlcmd/version.go
@@ -0,0 +1,40 @@
+/*
+Copyright 2023 Avi Zimmerman <avi.zimmerman@gmail.com>
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package ctlcmd
+
+import (
+	"github.com/spf13/cobra"
+
+	"github.com/webmeshproj/node/pkg/version"
+)
+
+func init() {
+	rootCmd.AddCommand(versionCmd)
+}
+
+var versionCmd = &cobra.Command{
+	Use:               "version",
+	Short:             "Print the version number of the CLI",
+	Args:              cobra.NoArgs,
+	ValidArgsFunction: completeNodes(1),
+	RunE: func(cmd *cobra.Command, args []string) error {
+		cmd.Println("Version:", version.Version)
+		cmd.Println("Git Commit:", version.Commit)
+		cmd.Println("Build Date:", version.BuildDate)
+		return nil
+	},
+}
