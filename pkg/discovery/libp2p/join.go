a0ac212d0d83f2705cb7dac665ac23b02b8a9f9a
Avi Zimmerman <avi.zimmerman@gmail.com>
2023-08-23 13:57:28 +0300

    feat: kad dht join discovery impl

--- pkg/discovery/libp2p/join.go
+++ pkg/discovery/libp2p/join.go
@@ -21,51 +21,41 @@ import (
 	"fmt"
 	"io"
 	"net"
+	"time"
 
 	"github.com/libp2p/go-libp2p"
 	dht "github.com/libp2p/go-libp2p-kad-dht"
-	"github.com/libp2p/go-libp2p/core/discovery"
 	"github.com/libp2p/go-libp2p/core/host"
-	"github.com/libp2p/go-libp2p/core/network"
 	drouting "github.com/libp2p/go-libp2p/p2p/discovery/routing"
-	dutil "github.com/libp2p/go-libp2p/p2p/discovery/util"
 
 	"github.com/webmeshproj/webmesh/pkg/context"
 	meshdiscovery "github.com/webmeshproj/webmesh/pkg/discovery"
 )
 
-// NewKadDHTAnnouncer creates a new announcer for the libp2p kademlia DHT.
-func NewKadDHTAnnouncer(opts *KadDHTOptions) (meshdiscovery.Discovery, error) {
+// NewKadDHTJoiner creates a new joiner for the libp2p kademlia DHT.
+func NewKadDHTJoiner(opts *KadDHTOptions) (meshdiscovery.Discovery, error) {
 	host, err := libp2p.New(opts.Options...)
 	if err != nil {
 		return nil, fmt.Errorf("libp2p new host: %w", err)
 	}
-	return &kadDHTAnnouncer{
+	return &kadDHTJoiner{
 		opts:    opts,
 		host:    host,
 		acceptc: make(chan io.ReadWriteCloser, 1),
 		closec:  make(chan struct{}),
-		stop:    func() {},
 	}, nil
 }
 
-// kadDHTAnnouncer is an announcer for the libp2p kademlia DHT.
-type kadDHTAnnouncer struct {
+// kadDHTJoiner is a joiner for the libp2p kademlia DHT.
+type kadDHTJoiner struct {
 	opts    *KadDHTOptions
 	host    host.Host
 	acceptc chan io.ReadWriteCloser
 	closec  chan struct{}
-	stop    func()
 }
 
-// Start starts the discovery service.
-func (kad *kadDHTAnnouncer) Start(ctx context.Context) error {
+func (kad *kadDHTJoiner) Start(ctx context.Context) error {
 	log := context.LoggerFrom(ctx).With("id", kad.host.ID())
-	kad.host.SetStreamHandler(JoinProtocol, func(s network.Stream) {
-		log.Debug("Handling join protocol stream", "peer", s.Conn().RemotePeer())
-		rw := bufio.NewReadWriter(bufio.NewReader(s), bufio.NewWriter(s))
-		kad.acceptc <- &kadStream{ReadWriter: rw, s: s}
-	})
 	kaddht, err := dht.New(ctx, kad.host)
 	if err != nil {
 		return fmt.Errorf("libp2p new dht: %w", err)
@@ -76,25 +66,55 @@ func (kad *kadDHTAnnouncer) Start(ctx context.Context) error {
 	}
 	log.Debug("Announcing join protocol with our PSK")
 	routingDiscovery := drouting.NewRoutingDiscovery(kaddht)
-	announceCtx, cancel := context.WithCancel(context.Background())
-	kad.stop = cancel
-	var opts []discovery.Option
-	if kad.opts.DiscoveryTTL > 0 {
-		opts = append(opts, discovery.TTL(kad.opts.DiscoveryTTL))
-	}
-	dutil.Advertise(announceCtx, routingDiscovery, kad.opts.PSK, opts...)
+	go kad.waitForPeers(ctx, routingDiscovery)
 	return nil
 }
 
+func (kad *kadDHTJoiner) waitForPeers(ctx context.Context, routingDiscovery *drouting.RoutingDiscovery) {
+	log := context.LoggerFrom(ctx).With("id", kad.host.ID())
+	peerChan, err := routingDiscovery.FindPeers(ctx, kad.opts.PSK)
+	if err != nil {
+		log.Error("Failed to find peers, retrying in 3 seconds", "error", err.Error())
+		select {
+		case <-kad.closec:
+			return
+		case <-time.After(3 * time.Second):
+			go kad.waitForPeers(ctx, routingDiscovery)
+		}
+	}
+	for peer := range peerChan {
+		if peer.ID == kad.host.ID() || len(peer.Addrs) == 0 {
+			continue
+		}
+		log.Debug("Found peer to join", "peer", peer.ID)
+		jctx, cancel := context.WithTimeout(ctx, 5*time.Second) // TODO: Make this configurable
+		s, err := kad.host.NewStream(jctx, peer.ID, JoinProtocol)
+		cancel()
+		if err != nil {
+			log.Warn("Failed to connect to peer", "peer", peer.ID, "error", err.Error())
+			continue
+		}
+		log.Debug("Connected to peer", "peer", peer.ID)
+		rw := bufio.NewReadWriter(bufio.NewReader(s), bufio.NewWriter(s))
+		kad.acceptc <- &kadStream{ReadWriter: rw, s: s}
+	}
+	log.Debug("peer channel exhausted, retrying in 3 seconds")
+	select {
+	case <-kad.closec:
+		return
+	case <-time.After(3 * time.Second):
+		go kad.waitForPeers(ctx, routingDiscovery)
+	}
+}
+
 // Stop stops the discovery service.
-func (kad *kadDHTAnnouncer) Stop() error {
+func (kad *kadDHTJoiner) Stop() error {
 	defer close(kad.closec)
-	kad.stop()
 	return kad.host.Close()
 }
 
 // Accept returns a connection to a peer.
-func (kad *kadDHTAnnouncer) Accept() (io.ReadWriteCloser, error) {
+func (kad *kadDHTJoiner) Accept() (io.ReadWriteCloser, error) {
 	select {
 	case <-kad.closec:
 		return nil, net.ErrClosed
