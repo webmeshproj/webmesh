7993c8265046e520a65cb72e16a1c9f34b1502f8
Avi Zimmerman <avi.zimmerman@gmail.com>
2023-09-26 12:57:11 +0300

    docs: update examples for new flags

--- pkg/storage/storage.go
+++ pkg/storage/storage.go
@@ -35,19 +35,6 @@ type DualStorage interface {
 	RaftStorage
 }
 
-// RaftStorage is the interface for storing and retrieving data about the state of the mesh.
-// This interface is used by mesh members that are part of the Raft cluster.
-type RaftStorage interface {
-	io.Closer
-	raft.LogStore
-	raft.StableStore
-
-	// Snapshot returns a snapshot of the storage.
-	Snapshot(ctx context.Context) (io.Reader, error)
-	// Restore restores a snapshot of the storage.
-	Restore(ctx context.Context, r io.Reader) error
-}
-
 // MeshStorage is the interface for storing and retrieving data about the state of the mesh.
 type MeshStorage interface {
 	io.Closer
@@ -67,6 +54,19 @@ type MeshStorage interface {
 	// Subscribe will call the given function whenever a key with the given prefix is changed.
 	// The returned function can be called to unsubscribe.
 	Subscribe(ctx context.Context, prefix string, fn SubscribeFunc) (context.CancelFunc, error)
+}
+
+// RaftStorage is the interface for storing and retrieving data about the state of the mesh.
+// This interface is used by mesh members that are part of the Raft cluster.
+type RaftStorage interface {
+	io.Closer
+	raft.LogStore
+	raft.StableStore
+
+	// Snapshot returns a snapshot of the storage.
+	Snapshot(ctx context.Context) (io.Reader, error)
+	// Restore restores a snapshot of the storage.
+	Restore(ctx context.Context, r io.Reader) error
 }
 
 // DropStorage is a storage interface that can be dropped entirely.
