f7b34c0baf6e02572dcfa371a08556eaaf322515
Avi Zimmerman <avi.zimmerman@gmail.com>
2023-10-26 01:42:40 +0300

    tests: add tls test for libp2p listener

--- pkg/meshnet/transport/libp2p/transport_test.go
+++ pkg/meshnet/transport/libp2p/transport_test.go
@@ -19,11 +19,13 @@ limitations under the License.
 package libp2p
 
 import (
+	"crypto/tls"
 	"testing"
 
 	v1 "github.com/webmeshproj/api/v1"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/credentials"
 	"google.golang.org/grpc/credentials/insecure"
 	"google.golang.org/grpc/status"
 
@@ -177,5 +179,62 @@ func TestRPCTransport(t *testing.T) {
 		})
 	})
 
-	t.Run("WithTLSCredentials", func(t *testing.T) {})
+	// The same tests as above but with doing an additional TLS upgrade.
+	t.Run("WithTLSCredentials", func(t *testing.T) {
+		t.Run("WithoutMTLS", func(t *testing.T) {
+			server, err := NewHost(ctx, HostOptions{})
+			if err != nil {
+				t.Fatal(err)
+			}
+			client, err := NewHost(ctx, HostOptions{
+				UncertifiedPeerstore: true,
+			})
+			if err != nil {
+				defer server.Close(ctx)
+				t.Fatal(err)
+			}
+			serverKey, serverCert, err := crypto.GenerateSelfSignedServerCert()
+			if err != nil {
+				defer server.Close(ctx)
+				defer client.Close(ctx)
+				t.Fatal(err)
+			}
+			tlsconf := &tls.Config{
+				InsecureSkipVerify: true,
+				Certificates: []tls.Certificate{{
+					Certificate: [][]byte{serverCert.Raw},
+					PrivateKey:  serverKey,
+				}},
+			}
+			srv := grpc.NewServer(grpc.Creds(credentials.NewTLS(tlsconf)))
+			t.Cleanup(srv.Stop)
+			v1.RegisterMeshServer(srv, v1.UnimplementedMeshServer{})
+			go func() {
+				err := srv.Serve(server.RPCListener())
+				if err != nil {
+					t.Log("Server error:", err)
+				}
+			}()
+			// Create a client transport.
+			rt := NewTransport(client, grpc.WithTransportCredentials(credentials.NewTLS(tlsconf)))
+			// Test the transport for each of the host's addresses.
+			defer client.Close(ctx)
+			for _, addr := range server.Host().Addrs() {
+				c, err := rt.Dial(ctx, server.ID(), addr.String())
+				if err != nil {
+					t.Fatal("Dial server address:", err)
+				}
+				defer c.Close()
+				cli := v1.NewMeshClient(c)
+				_, err = cli.GetNode(ctx, &v1.GetNodeRequest{})
+				// We should actually get an unimplemented error here.
+				if err == nil {
+					t.Fatal("Expected error, got nil")
+				}
+				if status.Code(err) != codes.Unimplemented {
+					t.Fatal("Expected unimplemented error, got", err)
+				}
+			}
+		})
+	})
 }
