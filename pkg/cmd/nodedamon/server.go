a0fb91a4103b4f7c2761730d1944ffbd82f121cf
Avi Zimmerman <avi.zimmerman@gmail.com>
2023-10-29 11:16:22 +0200

    fix: disable services was backwards in app daemon

--- pkg/cmd/nodedamon/server.go
+++ pkg/cmd/nodedamon/server.go
@@ -286,7 +286,7 @@ func (app *AppDaemon) buildConnConfig(ctx context.Context, req *v1.ConnectReques
 	conf.WireGuard.ListenPort = 0
 	conf.Global.LogLevel = app.conf.LogLevel
 	conf.Global.LogFormat = app.conf.LogFormat
-	conf.Services.API.Disabled = req.GetServices().GetEnabled()
+	conf.Services.API.Disabled = !req.GetServices().GetEnabled()
 	conf.Bootstrap.Enabled = req.GetBootstrap().GetEnabled()
 	if conf.Bootstrap.Enabled {
 		conf.Bootstrap.Admin = app.nodeID.String()
